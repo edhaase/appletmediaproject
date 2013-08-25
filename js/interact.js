d7ae87383fafb2056308bf94cf56430e022e3898
Edward Haase <ehaasecode@gmail.com>
2013-08-25 07:36:26 -0700

    Initial commit.

---
+++ js/interact.js
@@ -0,0 +1,7 @@
+function readyToShare() {
+	/* don't do anything at this time */
+}
+
+function errorAlert() {
+	document.getElementById("alertreceiver").innerHTML = "<div id='readyalert' class='alert alert-danger' data-alert='alert'><a class='close' href='#' data-dismiss='alert'>&times;</a><strong>Oh no!</strong> something has broken.</div>";
+}
